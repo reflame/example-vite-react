d2a0fba58f1e995644bfc8106fc50f00e403ba3d
Lewis Liu <lewisl9029@users.noreply.github.com>
2022-10-12 15:34:04 -0700

    Refactor out entry point
    Reflame uses entry.js/jsx/ts/tsx as the entry point, and it expects the entry point to export a single init function Reflame will call to initialize your app instead of executing side effects directly, in order to have better control over side effect execution for React Fast Refresh, and eventually for testing.
    
    To achive Vite compatibility, we can simply refactor the initialization code into an init function and then call it in the main.jsx entrypoint for Vite.
    
    Vite also only supports JSX in files with .jsx extension, while Reflame supports JSX in .js files too, so we need to rename everything to .jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { init } from './entry.jsx'
+import './global.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+init()
